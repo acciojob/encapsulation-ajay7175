372e79bfa03e91f33ab3c7fa1847769402524e96
Ajay Dattatray <56440284+ajay7175@users.noreply.github.com>
2023-10-28 14:22:07 +0530

    Update RWOnly.java

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String Name = "hey";
+
+
+    public String getName() {
+        return this.Name;
+    }
+
+    public void setName(String variable) {
+        this.Name = variable;
+    }
 }
