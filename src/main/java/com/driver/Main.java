7dec1bde4655e27e1d2ade52f032b773e8d84ae0
Ajay Dattatray <56440284+ajay7175@users.noreply.github.com>
2023-10-28 14:22:40 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
+  RWOnly obj = new RWOnly();
+
+//        obj.variable = "some value";
+  //java: variable has private access in com.driver.RWOnly
+  obj.setName("Updated");
+  System.out.println(obj.getName());
   
 }
